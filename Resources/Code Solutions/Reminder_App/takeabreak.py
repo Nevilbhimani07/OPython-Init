99ec2816986e5d6bd4c30f9a0e75dab743961f96
Nevilbhimani07 <164176438+Nevilbhimani07@users.noreply.github.com>
2024-11-13 13:27:01 -0500

    Update takeabreak.py

--- Resources/Code Solutions/Reminder_App/takeabreak.py
+++ Resources/Code Solutions/Reminder_App/takeabreak.py
@@ -6,5 +6,5 @@ print("This Program started on "+time.ctime())
 while(time_counter<total_time):
     time.sleep(10)
     webbrowser.open("https://www.youtube.com/watch?v=bmjYdc56gus")
-    time_counter+=1
+    time_counter+=1 
 
